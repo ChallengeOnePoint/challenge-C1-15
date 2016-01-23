db2cd4ca75cf4dd7d9dddbf3e7fd53bfc27f0027
Nicolas Leger <nicolas.leger@nleger.com>
2016-01-23 12:19:07 +0100

    Add Contacts controller

--- server/main.js
+++ server/main.js
@@ -23,8 +23,89 @@ var Contact = mongoose.model('Contact', {
   lastname: { type: String, maxlength: 100 }
 })
 
+/* Controllers */
+
+var router = express.Router();
+
+router.route('/api/contacts')
+  .get(function(req, res) {
+    Contact.find(function(err, contacts) {
+      if (err) {
+        return res.send(err);
+      }
+
+      res.json(contacts);
+    });
+  })
+  .post(function(req, res) {
+    var contact = new Contact(req.body);
+
+    contact.save(function(err) {
+      if (err) {
+        return res.send(err);
+      }
+
+      res.send({ message: 'Contact Added' });
+    });
+  });
+
+router.route('/contacts/:id')
+  .get(function(req, res) {
+    Contact.findOne({ _id: req.params.id}, function(err, contact) {
+      if (err) {
+        return res.send(err);
+      }
+
+      res.json(contact);
+    });
+  })
+  .put(function(req,res){
+    Contact.findOne({ _id: req.params.id }, function(err, contact) {
+      if (err) {
+        return res.send(err);
+      }
+
+      for (prop in req.body) {
+        contact[prop] = req.body[prop];
+      }
+
+      contact.save(function(err) {
+        if (err) {
+          return res.send(err);
+        }
+
+        res.json({ message: 'Contact updated!' });
+      });
+    });
+  })
+  .delete(function(req, res) {
+    Contact.remove({
+      _id: req.params.id
+    }, function(err, contact) {
+      if (err) {
+        return res.send(err);
+      }
+
+      res.json({ message: 'Successfully deleted' });
+    });
+  })
+
+
+router.get('/', function(req, res){
+  res.send('hello world');
+});
+
+app.use(router)
+
+/* Funky */
+
+app.get('/', function(req, res){
+  res.send('Welcome to El Contactor => Go speak API !');
+});
 
 /* Server */
 
 var server = http.createServer(app)
 server.listen(port)
+
+console.log('Hello world!')
