0cf661e38805ddf3daf33934b973a461971b2c37
Nicolas Leger <nicolas.leger@nleger.com>
2016-01-23 11:51:19 +0100

    Add contact model

--- server/main.js
+++ server/main.js
@@ -9,9 +9,22 @@ var bodyParser = require('body-parser')
 var path = require('path')
 var mongoose = require('mongoose')
 
+
 /* Database - Models */
 
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/el-contactor');
+
+var Contact = mongoose.model('Contact', {
+  number: { type: String, maxlength: 5 },
+  street: { type: String, maxlength: 150 },
+  city: { type: String, maxlength: 100 },
+  postcode: { type: String, maxlength: 8 },
+  firstname: { type: String, maxlength: 100 },
+  lastname: { type: String, maxlength: 100 }
+})
+
+
+/* Server */
 
 var server = http.createServer(app)
 server.listen(port)
